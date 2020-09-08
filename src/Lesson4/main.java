462e548d5e46d0bbe00dea0e204308a996da3412
TokioBoy <higemaster1@gmail.com>
2020-09-08 14:46:53 +0300

    Update

---
+++ src/Lesson4/main.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
