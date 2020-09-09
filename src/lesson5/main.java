f2e866b45c4649f79ea4bf48af618f6dfcbc7d49
TokioBoy <higemaster1@gmail.com>
2020-09-09 10:10:10 +0300

    nothing

---
+++ src/lesson5/main.java
@@ -0,0 +1,24 @@
+package lesson5;
+
+public class main {
+    public static void main(String[] args) {
+        byte y = 4;// 8
+        short z = 4;// 16
+        int price = 4;// 32
+        long x = 4;// 64
+        float p = (float) 1.5;// 32
+        double weight = 1.5;// 64
+
+        byte b = 4;
+        System.out.println(b);
+        byte b2= (byte)256;
+        System.out.println(b2);
+
+        String data =" I love Java";
+        String name = "Vlad";
+
+        String surname;
+        surname = "Guard";
+
+    }
+}
---
+++ src/lesson6/Main.java
@@ -0,0 +1,15 @@
+package lesson6;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 9;
+        System.out.println("x = " + x );
+        String name = "Vlad";
+        String surname ="Guard";
+        String fio = name + surname;
+        System.out.println(fio);
+
+    }
+}
