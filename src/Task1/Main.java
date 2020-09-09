57109c4a1c85893bb56f65c0eb18572218adfe9f
TokioBoy <higemaster1@gmail.com>
2020-09-09 11:04:50 +0300

    UPD

---
+++ src/Task1/Main.java
@@ -0,0 +1,15 @@
+package Task1;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 30;
+        int z = 50;
+        double xa = 100;
+        double ya = 130;
+        double za = 80;
+        double all = x * xa + y * ya + z * za;
+        System.out.println("Общая цена : " + all);
+
+    }
+}
--- src/lesson6/Main.java
+++ src/lesson6/Main.java
@@ -8,8 +8,27 @@ public class Main {
         System.out.println("x = " + x );
         String name = "Vlad";
         String surname ="Guard";
-        String fio = name + surname;
+        String fio = name + " " +  surname;
         System.out.println(fio);
 
+        System.out.println(x+y);
+        System.out.println(x + " " + y);
+        System.out.println(x + y + " " + y + x);
+
+        int x1 = 4;
+        int y1 = 5;
+        x1 +=4;
+        System.out.println(x1);
+
+        System.out.println(y1/x1);
+        System.out.println(y1 % x1);
+        double z1 = x1;
+
+        int number =725;
+        int h = number /100;
+        int temp = number % 100;
+        int d =temp/10;
+        int e= number / 10 ;
+        System.out.println("Сотен : " +  h + " ,десятков : " + d + ",едениц : " + e);
     }
 }
